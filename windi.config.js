6e610d681999e9088144634a9e87f243ef9aef96
adzharamrullah <adzharamrullah@gmail.com>
2021-10-12 14:24:31 +0700

    define design token

---
+++ windi.config.js
@@ -0,0 +1,16 @@
+
+export default {
+  theme: {
+    fontFamily: {
+      sans: ['Lato'],
+      arial: ['Roboto']
+    },
+    colors: {
+      white: '#FFF',
+      black: '#000',
+      green: {
+        700: '#069550'
+      }
+    }
+  }
+}
