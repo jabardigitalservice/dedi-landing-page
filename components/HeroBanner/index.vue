1f4b2b7d98fae3f0bbe0b32694597d68af15df20
adzharamrullah <adzharamrullah@gmail.com>
2021-10-14 15:11:50 +0700

    refactored hero banner and added motif banner

---
+++ components/HeroBanner/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hero-banner">
+    <div class="hero-banner__main" />
+    <div class="hero-banner__motif" />
+  </div>
+</template>
+
+<style lang="postcss">
+.hero-banner {
+  @apply relative h-[540px] sm:h-[590px];
+
+  &__main {
+    background-image: url('~/assets/images/HeroBanner.png');
+    background-position-x: 30%;
+    background-position-y: 100%;
+    @apply bg-no-repeat w-screen h-full;
+  }
+
+  &__motif{
+    background-image: url('~/assets/images/MotifHeroBanner.png');
+    @apply bg-no-repeat bg-cover w-full bottom-0 h-[57px] absolute;
+  }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,6 @@
 <template>
   <div>
     <Header />
-    <!-- example hero image, this will be refactor-->
-    <div class="hero-image" />
+    <HeroBanner />
   </div>
 </template>
-
-<style>
-.hero-image{
-  background-image: url('~/assets/images/HeroBanner.png');
-  background-repeat: no-repeat;
-  background-position-x: 40%;
-  width: 100vw;
-  height: 100vh;
-}
-</style>
