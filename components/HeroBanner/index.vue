31c4ae2f96ca3255bc4b5ec250f97cad88c8e6ac
adzharamrullah <adzharamrullah@gmail.com>
2021-10-15 00:14:47 +0700

    fix: responsive hero banner for large screen

--- components/HeroBanner/index.vue
+++ components/HeroBanner/index.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="hero-banner">
-    <div class="hero-banner__main" />
-    <div class="hero-banner__motif" />
+    <div class="hero-banner__main">
+      <h3>Judul Landing Page</h3>
+    </div>
   </div>
 </template>
 
 <style lang="postcss">
 .hero-banner {
-  @apply relative h-[540px] sm:h-[590px];
+  @apply relative h-[540px] sm:h-[590px] pt-[60px];
 
   &__main {
     background-image: url('~/assets/images/HeroBanner.png');
     background-position-x: 30%;
     background-position-y: 100%;
-    @apply bg-no-repeat w-screen h-full;
-  }
+    @apply bg-no-repeat bg-cover w-screen h-full;
 
-  &__motif{
-    background-image: url('~/assets/images/MotifHeroBanner.png');
-    @apply bg-no-repeat bg-cover w-full bottom-0 h-[57px] absolute;
+    &::before {
+      content: "";
+      background-image: url('~/assets/images/MotifHeroBanner.png');
+      background-size: calc(max(100%, 1440px)) auto;
+      background-position-y: 100%;
+      @apply bg-no-repeat w-full h-full absolute bottom-0;
+    }
   }
 }
 
