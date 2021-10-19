1e1eb664c16835891aa447d4914d0cd430298bd1
adzharamrullah <adzharamrullah@gmail.com>
2021-10-19 11:27:35 +0700

    chore : remove nuxt breakpoints

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,8 +61,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios', // nuxt axios
-    'nuxt-breakpoints' // nuxt breakpoints
+    '@nuxtjs/axios' // nuxt axios
   ],
 
   // Define breakpoints
