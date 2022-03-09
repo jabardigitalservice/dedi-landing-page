a5118a21b8c0b22f60054ccbe6d47cb46879d04c
Yoslie <yoslie.tan@gmail.com>
2022-03-09 11:29:53 +0700

    feat: Add section "Potensi (Visi)" to Page About in Landing Page Desa Digital

---
+++ components/Potency/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="potency" class="potency">
+    <div class="wrapper">
+      <div class="potency__content">
+        <div class="potency__content-img">
+          <img
+            height="473"
+            width="auto"
+            src="~assets/images/about/PotensiDesa.svg"
+            alt="Tim Pengembang Desa Digital"
+          >
+        </div>
+        <div class="potency__content-text">
+          <h1>Bayangkan Potensi Desa Berkembang Lebih Baik Dengan Teknologi Digital</h1>
+          <p>
+            Mempersempit kesenjangan digital adalah misi kami. Kami percaya, hadirnya teknologi bantu tingkatkan
+            produktivitas masyarakat desa, bahkan memperluas jangkauan produk usaha mereka.
+          </p>
+          <p>
+            Hal ini mendorong kami untuk ambil peran dalam pengembangan potensi desa dengan pemanfaatan teknologi yang
+            tepat. Demi terwujudnya kemandirian dan kesejahteraan masyarakat secara inklusif. Karena teknologi hadir
+            untuk semua.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentPotency'
+}
+
+</script>
+
+<style lang="postcss">
+  .potency {
+    @apply w-full h-full py-6 bg-[#F8F8F6]
+      md: (pt-15 pb-30)
+      lg: (py-26);
+
+    &__content {
+      @apply w-full h-full flex flex-col-reverse
+      md: (grid grid-cols-2 justify-between items-center)
+      lg: (grid grid-cols-2 justify-between items-center);
+
+      &-img {
+        @apply w-full h-full flex;
+      }
+
+      &-text {
+        @apply w-full h-full flex flex-col
+        md: (py-30);
+
+        h1 {
+          @apply w-full h-full text-[28px] text-center leading-8 font-serif font-bold text-blue-gray-800
+          md: (px-2 text-left leading-10)
+          lg: (text-[42px] text-left leading-14);
+        }
+
+        p {
+          @apply w-full h-full pt-6 text-center text-blue-gray-500
+          md: (w-full text-left px-2)
+          lg: (w-[586px] text-left);
+        }
+      }
+    }
+  }
+
+</style>
