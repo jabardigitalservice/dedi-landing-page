b76151a27766c73e45f30d3d2fd09f1a4662f6ea
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-03-18 16:01:07 +0700

    feat:  wip page registration partner

---
+++ pages/registration.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-[calc(100vh-60px)] bg-[#32E6F9] flex items-end">
+    <img src="~/assets/images/FooterBanner.svg" alt="Logo Desa Digital">
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'registration'
+}
+</script>
+
+<style>
+
+</style>
