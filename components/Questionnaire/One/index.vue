7c32398d5fad786e66bc60f1ee1d7c57fc01db4d
Yoslie <yoslie.tan@gmail.com>
2022-04-13 10:51:25 +0700

    refactor: update endpoints files upload

--- components/Questionnaire/One/index.vue
+++ components/Questionnaire/One/index.vue
@@ -452,9 +452,10 @@ export default {
     },
     submitFile (image) {
       return new Promise((resolve, reject) => {
-        this.$axios.post(`/files/upload?secret=${this.uploadFileSecret}`, image, {
+        this.$axios.post('/files/upload', image, {
           headers: {
-            'Content-Type': 'multipart/form-data'
+            'Content-Type': 'multipart/form-data',
+            'x-api-key': this.uploadFileSecret
           }
         }).then((response) => {
           const { data } = response.data
