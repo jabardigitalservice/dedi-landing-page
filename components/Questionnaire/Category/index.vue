8b425a11fa936b1ac07a6f5c59d443e9ab0dbee3
Doohan Aditya Sapuro <doohanaditya@yahoo.co.id>
2022-05-12 14:56:17 +0700

    refactor (questionnaire-category): add margin top and bottom

--- components/Questionnaire/Category/index.vue
+++ components/Questionnaire/Category/index.vue
@@ -93,8 +93,8 @@ export default {
 
 <style lang="postcss" scoped>
 .category {
-  @apply grid grid-rows-1 p-4
-  sm:(p-8 bg-white w-[680px] h-[486px] rounded-2xl);
+  @apply grid grid-rows-1 p-4 mt-20
+  sm:(my-[70px] p-8 bg-white w-[680px] h-[486px] rounded-2xl);
 
   &__title > h1 {
     @apply font-roboto font-bold text-[16px] leading-[22px] text-gray-800 pb-4
