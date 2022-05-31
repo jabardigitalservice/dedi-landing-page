849bf96207b204fc958f54a996772c528c6ab495
Doohan Aditya Sapuro <doohanaditya@yahoo.co.id>
2022-05-31 11:08:10 +0700

    refactor (modal-criteria-mitra): remove index and style for modal criteria mitra

--- components/CriteriaMitra/index.vue
+++ components/CriteriaMitra/index.vue
@@ -1,3 +1,66 @@
+<template>
+  <div
+    :class="{
+      'modal__backdrop': true,
+      'modal__backdrop--show': show,
+      'modal__backdrop--hide': !show,
+    }"
+    @click.self="onClose('overlay')"
+  >
+    <div
+      :class="{
+        'modal__wrapper': true,
+        'modal__wrapper--show': show,
+        'modal__wrapper--hide': !show,
+      }"
+    >
+      <div class="modal__close">
+        <BaseButton variant="circle" @click="onClose">
+          <template #icon>
+            <jds-icon class="text-white" size="20px" name="times" />
+          </template>
+        </BaseButton>
+      </div>
+      <div class="modal__container">
+        <div class="modal__container-heading">
+          Kriteria Mitra Yang Dapat Mengikuti
+        </div>
+        <div class="modal__container-join-as">
+          <!-- @todo: add content in the next pr -->
+        </div>
+        <div class="modal__container-button">
+          <BaseButton variant="primary" label="Oke, saya mengerti" @click="onClose" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      selected: null
+    }
+  },
+  methods: {
+    onClose (closeOn) {
+      if (closeOn !== 'overlay' || window.innerWidth < 640) {
+        this.selected = null
+        this.$emit('onCloseModal', false)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
 .modal {
 
   &__backdrop {
@@ -62,3 +125,5 @@
     @apply absolute right-4 -top-15 sm:hidden;
   }
 }
+
+</style>
