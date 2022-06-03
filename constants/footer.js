51f1c12de52dd7d71a8a1c91e8305e21a2aaa23a
Doohan Aditya Sapuro <doohanaditya@yahoo.co.id>
2022-06-03 15:24:59 +0700

    chore (conststans): add variables for content in footer

---
+++ constants/footer.js
@@ -0,0 +1,65 @@
+export const mainFooters = [
+  {
+    id: 1,
+    element: 'header',
+    text: 'Tentang Desa Digital'
+  },
+  {
+    id: 2,
+    element: 'village-partner',
+    text: 'Data Perkembangan Desa Digital'
+  },
+  {
+    id: 3,
+    element: 'testimonial',
+    text: 'Testimoni User'
+  },
+  {
+    id: 4,
+    element: 'join-info',
+    text: 'Cara Bergabung'
+  },
+  {
+    id: 5,
+    element: 'contact-us',
+    text: 'Kontak Kami'
+  }
+]
+
+export const competitionFooters = [
+  {
+    id: 1,
+    element: 'competition-banner',
+    text: 'Gabung Sayembara Desa Digital'
+  },
+  {
+    id: 2,
+    element: 'competition-about',
+    text: 'Tentang Sayembara Desa Digital'
+  },
+  {
+    id: 3,
+    element: 'village-criteria',
+    text: 'Kriteria Desa'
+  },
+  {
+    id: 4,
+    element: 'criteria-mitra',
+    text: 'Kriteria Mitra'
+  },
+  {
+    id: 5,
+    element: 'competition-flow',
+    text: 'Alur Sayembara Desa Digital'
+  },
+  {
+    id: 6,
+    element: 'competition-join',
+    text: 'Gabung Desa Digital'
+  },
+  {
+    id: 7,
+    element: 'contact-us',
+    text: 'Kontak Kami'
+  }
+]
