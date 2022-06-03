e2ca53ad32032a5d7fd89dbc6ce4899bf5214441
Doohan Aditya Sapuro <doohanaditya@yahoo.co.id>
2022-06-03 15:48:14 +0700

    chore (footer-about-page): add new variables

--- constants/footer.js
+++ constants/footer.js
@@ -63,3 +63,46 @@ export const competitionFooters = [
     text: 'Kontak Kami'
   }
 ]
+
+export const aboutFooters = [
+  {
+    id: 1,
+    element: 'about-banner',
+    text: 'Gabung Desa Digital'
+  },
+  {
+    id: 2,
+    element: 'potency',
+    text: 'Potensi Desa Digital'
+  },
+  {
+    id: 3,
+    element: 'achievement',
+    text: 'Penghargaan Desa Digital'
+  },
+  {
+    id: 4,
+    element: 'develop-stage',
+    text: 'Tahap Perkembangan Desa Digital'
+  },
+  {
+    id: 5,
+    element: 'three-pilars',
+    text: 'Tiga Pilar Desa Digital'
+  },
+  {
+    id: 6,
+    element: 'team',
+    text: 'Tim Pengembang Desa Digital'
+  },
+  {
+    id: 7,
+    element: 'they-said',
+    text: 'Testimoni '
+  },
+  {
+    id: 8,
+    element: 'contact-us',
+    text: 'Kontak Kami'
+  }
+]
