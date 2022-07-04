0e43ef68cebc7a7d6ce0809f363ba9ea55ffd44b
Yoslie <yoslie.tan@gmail.com>
2022-07-04 09:59:08 +0700

    style: fix wrapper and content text style hero banner page about

--- components/AboutBanner/index.vue
+++ components/AboutBanner/index.vue
@@ -1,24 +1,22 @@
 <template>
-  <div id="about-banner" class="wrapper">
-    <div class="about-banner">
-      <div class="about-banner__wrapper">
-        <div class="about-banner__info">
-          <h2 class="about-banner__info-title">
-            Meniadakan Batasan Untuk
-          </h2>
-          <h2 class="about-banner__info-subtitle">
-            Membawa Perubahan
-          </h2>
-          <p class="about-banner__info-desc">
-            Perluas jangkauan, kembangkan kapasitas masyarakat, dan
-            maksimalkan potensi desa.
-          </p>
-          <div class="about-banner__info-cta">
-            <BaseButton class="about-banner__info-cta-btn" label="Gabung Desa Digital" @click="onClickCTA" />
-          </div>
+  <div id="about-banner" class="about-banner">
+    <div class="about-banner__wrapper">
+      <div class="about-banner__info">
+        <h2 class="about-banner__info-title">
+          Meniadakan Batasan Untuk
+        </h2>
+        <h2 class="about-banner__info-subtitle">
+          Membawa Perubahan
+        </h2>
+        <p class="about-banner__info-desc">
+          Perluas jangkauan, kembangkan kapasitas masyarakat, dan
+          maksimalkan potensi desa.
+        </p>
+        <div class="about-banner__info-cta">
+          <BaseButton class="about-banner__info-cta-btn" label="Gabung Desa Digital" @click="onClickCTA" />
         </div>
-        <img class="about-banner__image" src="~/assets/images/AboutBanner.png" alt="About Banner">
       </div>
+      <img class="about-banner__image" src="~/assets/images/AboutBanner.png" alt="About Banner">
     </div>
   </div>
 </template>
@@ -35,24 +33,24 @@ export default {
 </script>
 <style lang="postcss" scoped>
 .about-banner {
-  @apply pt-[60px] pb-6 lg:(pb-[unset]);
+  @apply max-w-7xl pt-[60px] pb-6 md:(mx-8) lg:(pb-[unset]) xl:(mx-auto);
 
   &__wrapper {
     @apply flex flex-col-reverse lg:(flex-row justify-between items-center);
   }
 
   &__info {
-    @apply block lg:(w-[443px]) xl:(w-[unset]);
+    @apply block mx-4 lg:(w-[443px]) xl:(w-[unset]);
 
     &-title {
-      @apply font-serif text-center text-[20px] leading-[40px]
+      @apply font-serif text-center text-[28px] leading-normal
       text-blue-gray-700 font-bold mt-6
       lg:(mt-[unset] text-left text-[24px] leading-[45px])
       xl:(text-[38px]);
     }
 
     &-subtitle {
-      @apply font-serif text-center text-[20px] leading-[40px]
+      @apply font-serif text-center text-[28px] leading-normal
       text-blue-gray-700 font-bold
       lg:(mt-[unset] text-left text-[40px] leading-[46.88px])
       xl:(text-[56px] leading-[65.63px]);
