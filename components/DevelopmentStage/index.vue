fb87a8dbeae8bae0781a3ac80355bcaffd313df3
Doohan Aditya Sapuro <doohanaditya@yahoo.co.id>
2022-07-06 10:09:33 +0700

    style (development-stage): change width pagination-dot-active to 32px

--- components/DevelopmentStage/index.vue
+++ components/DevelopmentStage/index.vue
@@ -260,7 +260,7 @@ export default {
       @apply float-left mr-[12px] w-3 h-3 rounded-full bg-gray-300 transition-all duration-300 ease-in-out;
 
       &--active {
-        @apply w-[44px] cursor-auto bg-green-600;
+        @apply w-[32px] cursor-auto bg-green-600;
       }
     }
 
