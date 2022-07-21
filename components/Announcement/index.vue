e2c445d1874a5a2b9741250f432a27c9477a28e6
Doohan Aditya Saputro <doohanaditya@yahoo.co.id>
2022-07-21 13:05:45 +0700

    feat (announcement-competition): create template announcement modal

---
+++ components/Announcement/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <Portal to="announcement">
+    <div
+      :class="{
+        'modal__backdrop': true,
+        'modal__backdrop--show': show,
+        'modal__backdrop--hide': !show,
+      }"
+      @click.self="onClose('overlay')"
+    >
+      <div
+        :class="{
+          'modal__wrapper': true,
+          'modal__wrapper--show': show,
+          'modal__wrapper--hide': !show,
+        }"
+      >
+        <div class="modal__container">
+          <div class="modal__container-image">
+            <BaseButton variant="circle">
+              <template #icon>
+                <jds-icon class="text-white" size="20px" name="times" />
+              </template>
+              <img class="" src="~/assets/images/competition/Announcement_competition.svg">
+            </BaseButton>
+          </div>
+          <div class="modal__container-heading">
+            <h1>
+              Selamat Kepada Desa Terpilih Di Sayembara Desa Digital 🎉
+            </h1>
+          </div>
+          <div class="modal__container-subheading">
+            <p>
+              Kami ucapkan terima kasih atas partisipasi wargi yang telah mendaftar sebagai desa penerima manfaat sayembara di tahun 2022.
+            </p>
+          </div>
+          <div class="modal__container-button">
+            <BaseButton class="modal__container-button-announcement" :variant="btnVariant" label="Cek Hasil Sayembara">
+              <template #icon>
+                <jds-icon class="text-green-700" size="18px" name="cloud-download" />
+              </template>
+            </BaseButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Portal>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
