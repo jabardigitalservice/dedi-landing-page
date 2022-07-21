54721b57367df1555e904267894a67ca8edb3cd7
Doohan Aditya Saputro <doohanaditya@yahoo.co.id>
2022-07-21 15:46:21 +0700

    style (announcement): add style for announcement modal

--- components/Announcement/index.vue
+++ components/Announcement/index.vue
@@ -15,30 +15,30 @@
           'modal__wrapper--hide': !show,
         }"
       >
-        <div class="modal__container">
-          <div class="modal__container-image">
-            <BaseButton variant="circle">
-              <template #icon>
-                <jds-icon class="text-white" size="20px" name="times" />
-              </template>
-              <img class="" src="~/assets/images/competition/Announcement_competition.svg">
-            </BaseButton>
+        <div class="modal__box">
+          <div class="modal__box-image">
+            <button class="modal__box--circle">
+              <IconTimes
+                class="icon__times"
+              />
+            </button>
+            <img class="w-full rounded-t-[15px]" width="681" height="255" src="~/assets/images/competition/Announcement_competition.svg">
           </div>
-          <div class="modal__container-heading">
+          <div class="modal__box-heading">
             <h1>
-              Selamat Kepada Desa Terpilih Di Sayembara Desa Digital 🎉
+              Selamat Kepada Desa Terpilih Di Sayembara Desa Digital
+              <img class="inline-block" width="28" height="28" src="~/assets/icons/IconParty.svg" alt="icon party">
             </h1>
           </div>
-          <div class="modal__container-subheading">
-            <p>
+          <div class="modal__box-subheading">
+            <p class="modal__box-subheading--blue">
               Kami ucapkan terima kasih atas partisipasi wargi yang telah mendaftar sebagai desa penerima manfaat sayembara di tahun 2022.
             </p>
           </div>
-          <div class="modal__container-button">
-            <BaseButton class="modal__container-button-announcement" :variant="btnVariant" label="Cek Hasil Sayembara">
-              <template #icon>
-                <jds-icon class="text-green-700" size="18px" name="cloud-download" />
-              </template>
+          <div class="modal__box-button">
+            <BaseButton class="modal__box-button-announcement" variant="primary">
+              <jds-icon class="text-white" size="18px" name="cloud-download" />
+              <span>Cek Hasil Sayembara</span>
             </BaseButton>
           </div>
         </div>
@@ -48,7 +48,10 @@
 </template>
 
 <script>
+import IconTimes from '~/assets/icons/IconTimes.svg?inline'
+
 export default {
+  components: { IconTimes },
   props: {
     show: {
       type: Boolean,
@@ -57,3 +60,101 @@ export default {
   }
 }
 </script>
+
+<style lang="postcss">
+.modal {
+
+  &__backdrop {
+    @apply z-20 fixed h-screen w-screen backdrop-filter backdrop-blur-[6px]
+    bg-black bg-opacity-75 top-0 transition-all duration-300;
+
+    &--show {
+      @apply visible opacity-100;
+    }
+
+    &--hide{
+      @apply invisible opacity-0;
+    }
+  }
+
+  &__wrapper{
+    @apply fixed bottom-0 w-full transition-all duration-300
+    sm:(relative top-1/2 mx-auto my-0 bottom-[unset] max-w-[680px]);
+
+    &--show {
+      @apply transform translate-y-0 sm:(-translate-y-1/2);
+    }
+
+    &--hide{
+      @apply transform translate-y-[20%] sm:(-translate-y-[30%]);
+    }
+  }
+
+  &__box {
+    @apply bg-white rounded-t-2xl flex flex-col rounded-2xl relative;
+
+    &--circle {
+      @apply w-[38px] h-[38px] rounded-full bg-white absolute flex justify-center items-center right-2 top-2;
+    }
+
+    &-heading {
+      @apply h-full max-w-[425px] mx-auto pt-3 px-8 font-roboto font-bold text-[28px] leading-[42px] text-green-700 text-center
+      sm:(px-4);
+    }
+
+    &-subheading {
+      @apply h-full max-w-[621px] mx-auto mt-4 px-8 text-center
+      sm:(px-4);
+
+      &--blue {
+        @apply text-sm leading-[23px] font-sans text-blue-gray-800 font-normal;
+      }
+    }
+
+    &-image {
+      @apply gap-3 grid-cols-2 justify-between
+      sm:(gap-6 max-h-[257px] flex items-center justify-center);
+    }
+
+    &-info {
+      @apply min-w-[250px] max-w-[725px] flex justify-center justify-items-start items-start gap-4 mt-6 py-3.5 px-4 bg-blue-50 border border-blue-800 rounded-lg
+      sm:(mx-4)
+      md:(mx-6);
+
+     &-content {
+        @apply flex-grow;
+
+        &-text {
+          @apply items-start text-[14px] leading-[23px];
+
+          &--bold {
+            @apply text-gray-900 font-bold;
+          }
+        }
+      }
+    }
+
+    &-button {
+      @apply w-full flex justify-center mt-4 mb-6;
+
+      &-close {
+        @apply !hidden
+        sm:(!inline-block w-[165px] h-[38px] !py-1 active:(!my-0 w-[calc(165px-2px)]));
+      }
+
+      &-announcement {
+        @apply !py-2 !px-4;
+      }
+    }
+  }
+
+  &__close {
+    @apply absolute right-4 -top-15 sm:hidden;
+  }
+}
+
+.icon__times path {
+  @apply fill-green-700;
+}
+
+</style>
